355f931ae90707de010ad38e1bba71ea53ce6539
Gaia26 <106587700+Gaia26@users.noreply.github.com>
2022-06-09 19:01:48 +0200

    Update part_3.py

--- part_3.py
+++ part_3.py
@@ -13,23 +13,23 @@ def index_page():
 
 @app.route('/MD') #metadata 
 def first_page():
-	output = Registry().link_metadata()
-	return render_template('metadata.html', result = output)
+	output = Registry().link_metadata() # link from part 1
+	return render_template('metadata.html', result = output) # html template
 
 @app.route('/Sem') #general semantics
 def second_page():
-	output = Registry().link_semantics() # link from part 1
-	return render_template('general_semantics.html', result = output) # html = template
+	output = Registry().link_semantics() 
+	return render_template('general_semantics.html', result = output) 
 
 @app.route('/G') #genes
 def third_page():
 	output = Registry().link_geneNum()
 	return render_template('genes.html', result = output)
 
-@app.route('/P4')
+@app.route('/P4') # input for classes 4 
 def fourthuser_page():
 	output = Registry().link_prefour()
-	return render_template('user_genesentences.html', result = output) # input
+	return render_template('user_genesentences.html', result = output) 
 
 
 @app.route('/SenG') #sentences genes
@@ -42,7 +42,7 @@ def fifth_page():
 	output = Registry().link_diseaseNum()
 	return render_template('diseases.html', result = output)
 
-@app.route('/P6')
+@app.route('/P6') # input for class 6 
 def sixthuser_page():
 	output=Registry().link_presixth()
 	return render_template('user_diseasesentences.html', result = output)
@@ -58,9 +58,9 @@ def seventh_page():
 	return render_template('top_ten.html', result = output)
 
 
-@app.route('/P8')
+@app.route('/P8') # input for class 8
 def eighthuser_page():
-	output=Registry().link_prefour()
+	output=Registry().link_prefour() 
 	return render_template('user_usereight.html', result = output)
 
 
@@ -70,7 +70,7 @@ def eight_page():
 	return render_template('association_d.html', result = output)
 
 
-@app.route('/P9')
+@app.route('/P9') # input for class 9
 def ninthuser_page():
 	output=Registry().link_presixth()
 	return render_template('user_nine.html', result = output)
